85078879b2576d3a376a243a29d0929fafd1a06b
Moises <kenydigital2@gmail.com>
2024-12-18 00:13:02 +0000

    Project created

---
+++ StartupWebMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StartupWebMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
